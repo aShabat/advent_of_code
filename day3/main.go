8e51e49f1b37d43d40712c01e3037442eeba181f
Anton Shabat <anton.shabat@gmail.com>
2024-12-03 08:30:08 +0200

    day3

---
+++ day3/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"advent/util"
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	lines, close := util.ReadFile("day3/input.txt")
+	close()
+
+	rx := regexp.MustCompile(`don't\(\)|do\(\)|mul\((\d+),(\d+)\)`)
+	sum1 := 0
+	sum2 := 0
+	line := ""
+	for _, l := range lines {
+		line += l
+	}
+	do := true
+	for _, match := range rx.FindAllStringSubmatch(line, -1) {
+		if match[0] == "do()" {
+			do = true
+		} else if match[0] == "don't()" {
+			do = false
+		} else {
+			left, err := strconv.Atoi(match[1])
+			if err != nil {
+				panic(err)
+			}
+			right, err := strconv.Atoi(match[2])
+			if err != nil {
+				panic(err)
+			}
+			sum1 += left * right
+			if do {
+				sum2 += left * right
+			}
+		}
+	}
+
+	fmt.Println(sum2)
+}
