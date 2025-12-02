e83793426c25a03e12da84eb2938b1edfbb603dd
Anton Shabat <anton.shabat@gmail.com>
2025-12-02 18:15:51 +0200

    some util tweaks

--- 2025_clojure/util.clj
+++ 2025_clojure/util.clj
@@ -1,11 +1,12 @@
 (ns util
-  (:require [clojure.string :refer [upper-case split]]
+  (:require [clojure.string :refer [upper-case split trim]]
             [clojure.java.shell :refer [sh]]
             [clojure.java.io :refer [file]]))
 
 (defn echo [x] (prn (str "ECHO: " x)) x)
 
 (def aoc-session "53616c7465645f5f009a193d45549a164835a9d33341fd84a2923f27294073dfb82735276f3a6e50b46478366b73999f20a83f31c0e5303ddb280b96bdc7e658")
+(def year 2025)
 (def http-client (java.net.http.HttpClient/newHttpClient))
 
 (defn http-request
@@ -21,50 +22,50 @@
          (.header "Content-Type" "application/x-www-form-urlencoded")
          (.build)))))
 
-(defn http-send
+(defn- http-send
   [request]
   (let [response (.send http-client request (java.net.http.HttpResponse$BodyHandlers/ofString))]
     {:body (.body response) :status (.statusCode response)}))
 
-(defn aoc-request [method body & args]
+(defn- aoc-request [method body & args]
   (let [aoc-url (apply str "https://adventofcode.com" (map #(str "/" %) args))]
     (http-request aoc-url method body)))
 
-(defn html-extract-main [html] (get (re-find #"(?s)<main>(.*)</main>" html) 1))
+(defn- html-extract-main [html] (get (re-find #"(?s)<main>(.*)</main>" html) 1))
 
-(defn dir-exists? [path]
+(defn- dir-exists? [path]
   (let [f (file path)]
     (when-not (.isDirectory f) (.mkdirs f))))
 
-(defn aoc-get-exercise-html [year day]
+(defn- aoc-get-exercise-html [day]
   (let [request (aoc-request "get" nil year "day" day)
         response (http-send request)]
     (html-extract-main (:body response))))
 
-(defn convert-file [file from to] (sh "pandoc" "-o" (str file to) (str file from)))
+(defn- convert-file [file from to] (sh "pandoc" "-o" (str file to) (str file from)))
 
-(defn aoc-get-exercise [year day]
-  (let [html (aoc-get-exercise-html year day)
+(defn aoc-get-exercise [day]
+  (let [html (aoc-get-exercise-html day)
         dir (str "static/" day)
         file (str dir "/exercise")]
     (dir-exists? dir)
     (spit  (str file ".html") html)
     (convert-file file ".html" ".md")))
 
-(defn aoc-send-answer-html [year day part answer]
+(defn- aoc-send-answer-html [day part answer]
   (let [body (str "level=" part "&answer=" answer)
         request (aoc-request "post" body year "day" day "answer")
         response (http-send request)]
     (html-extract-main (:body response))))
 
-(defn aoc-send-answer [year day part answer]
-  (let [html (aoc-send-answer-html year day part answer)]
+(defn aoc-send-answer [day part answer]
+  (let [html (aoc-send-answer-html day part answer)]
     (condp re-find html
-      #"(?s)That's the right answer" (do (prn "Right answer") (aoc-get-exercise year day))
+      #"(?s)That's the right answer" (do (prn "Right answer") (aoc-get-exercise day))
       #"(?s)That's not the right answer" :wrong
       #"(?s)You gave an answer too recently" :too-soon)))
 
-(defn aoc-get-input [year day]
+(defn aoc-get-input [day]
   (let [request (aoc-request "get" nil year "day" day "input")
         response (http-send request)
         dir (str "static/" day)
@@ -72,5 +73,11 @@
     (dir-exists? dir)
     (spit file (:body response))))
 
-(defn split-input [input]
-  (map #(split % #" ") (split input #"\n")))
+(defn read-input [day]
+  (let [file (str "static/" day "/input.txt")]
+    (trim (slurp file))))
+
+(defn lines [input] (split input #"\n"))
+
+(defmacro defnm [name params & body]
+  `(def ~name (memoize (fn ~params ~@body))))
