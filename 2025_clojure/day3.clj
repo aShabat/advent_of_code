ecd24bc0c2486b0b4fa8bee0533b5628c50ffa8b
Anton Shabat <anton.shabat@gmail.com>
2025-12-04 07:04:05 +0200

    added stack-based solution(3 times faster) and fixed some uncommitted problems

--- 2025_clojure/day3.clj
+++ 2025_clojure/day3.clj
@@ -18,8 +18,46 @@
 (defn bank-joltage
   [bank n]
   (nth (reduce (fn [acc digit]
-               (map max acc (cons digit (map #(+ digit (* 10 %)) acc))))
-          (repeat n 0)
-          bank) n)
+                 (map max acc (cons digit (map #(+ digit (* 10 %)) acc))))
+               (repeat n 0)
+               bank)
+       (dec n)))
 
-(bank-joltage [1 2 3] 2)
+(defn bank-joltage-stack
+  [bank n]
+  (loop [stack '()
+         stack-length 0
+         bank-tail bank
+         bank-tail-length (count bank)]
+    (cond (zero? bank-tail-length) (reduce #(+ (* 10 %1) %2) (reverse stack))
+          (and (not (zero? stack-length))
+               (> (first bank-tail) (first stack))
+               (> (+ stack-length bank-tail-length) n))
+          (recur (rest stack) (dec stack-length) bank-tail bank-tail-length)
+          (< stack-length n) (recur (cons (first bank-tail) stack)
+                                    (inc stack-length)
+                                    (rest bank-tail)
+                                    (dec bank-tail-length))
+          :else
+          (recur stack stack-length (rest bank-tail) (dec bank-tail-length)))))
+
+(defn part-1
+  [input]
+  (->> input
+       (parse-input)
+       (map #(bank-joltage % 2))
+       (reduce +)))
+
+(defn part-2
+  [input]
+  (->> input
+       (parse-input)
+       (map #(bank-joltage % 12))
+       (reduce +)))
+
+(defn part-2-stack
+  [input]
+  (->> input
+       (parse-input)
+       (map #(bank-joltage-stack % 12))
+       (reduce +)))
